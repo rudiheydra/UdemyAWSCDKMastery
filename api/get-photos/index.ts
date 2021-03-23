415c34c169b1e92435c0f7a76188ebc4f3bfaeb8
Rudi Heydra <53549619+rudiheydra@users.noreply.github.com>
2021-03-23 19:17:02 +1100

    first commit

---
+++ api/get-photos/index.ts
@@ -0,0 +1,18 @@
+import { 
+    APIGatewayProxyEventV2, 
+    Context,
+    APIGatewayProxyStructuredResultV2
+} from 'aws-lambda';
+
+
+async function getPhotos(
+    event: APIGatewayProxyEventV2, 
+    context: Context
+    ): Promise <APIGatewayProxyStructuredResultV2> {
+     return {
+       statusCode: 200,
+       body: 'Hello from lambda, it is live!',
+    };
+}
+
+export {getPhotos};
--- lib/simple-app-stack.ts
+++ lib/simple-app-stack.ts
@@ -1,9 +1,26 @@
+import { Bucket, BucketEncryption } from '@aws-cdk/aws-s3';
+import * as lambda from '@aws-cdk/aws-lambda-nodejs'
 import * as cdk from '@aws-cdk/core';
+import { Runtime } from '@aws-cdk/aws-lambda';
+import * as path from 'path';
 
 export class SimpleAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const bucket = new Bucket(this, 'MySimpleAppBucket' ,{
+      encryption: BucketEncryption.S3_MANAGED,
+    });
+
+    const getPhotos = new lambda.NodejsFunction(this, 'MySimpleAppLambda', {
+      runtime: Runtime.NODEJS_10_X,
+      entry: path.join(__dirname, '..', 'api', 'get-photos', 'index.ts'),
+      handler: 'getPhotos',
+    })
+
+    new cdk.CfnOutput(this, 'MySimpleAppBucketNameExport', {
+      value: bucket.bucketName,
+      exportName: 'MySimpleAppBucketName',
+    });
   }
 }
